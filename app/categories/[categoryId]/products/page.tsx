25d3a920defccdf46865c596fe8bf54fc27ac6d8
gmaxstores <maxwellnvani@gmail.com>
2025-10-01 20:49:48 +0100

    removed the reviews star display

--- app/categories/[categoryId]/products/page.tsx
+++ app/categories/[categoryId]/products/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "@ui/products-listing.module.css";
 import Link from "next/link";
 import { getReviewsByProductId, getCategoryById } from "../../../lib/data";
-import AverageRating from "@ui/average-rating";
+//import {AverageRating} from "@ui/average-rating";
 
 
 
@@ -37,16 +37,16 @@ export default async function Page({ params }: { params: { categoryId: string}})
             <div className={styles.productsListingLinksDiv}>
                 {products.map(product => {
                     // display reviews as stars
-                    const productReviews = reviews[products.findIndex(p => p.id === product.id)] || [];
-                    const numReviews = productReviews.length > 0 ? productReviews.length : 0;
-                    const averageRating =
-                        productReviews.length > 0
-                            ? productReviews.reduce((sum, r) => sum + r.rating, 0) / productReviews.length
-                            : 0;
+                    // const productReviews = reviews[products.findIndex(p => p.id === product.id)] || [];
+                    // const numReviews = productReviews.length > 0 ? productReviews.length : 0;
+                    // const averageRating =
+                    //     productReviews.length > 0
+                    //         ? productReviews.reduce((sum, r) => sum + r.rating, 0) / productReviews.length
+                    //         : 0;
                     return (
                         <Link key={product.id} className={styles.productsListingLink} href={`/products/${product.id}/product`}>
                             <Image className={styles.productsListingLinkImages} src={product.image_url} alt={product.name} width={200} height={200} />
-                            {AverageRating(averageRating, numReviews)}
+                            {/* <AverageRating rating={averageRating} numReviews={numReviews} totalStars={5} /> */}
                             <h2>{product.name}</h2>
                             <p>{product.description}</p>
                             <p className={styles.productsListingLinkPrice}>Price: ${product.price}</p>
